9cb602af07d1cf7e7264dd74070c284ff55025e3
Lucas Souza <ibmstemp@gmail.com>
2019-08-31 13:42:59 -0300

    layout 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  
+  def component
+   
+  end
 
   def home
+    @cta = 'shared/components/cta'
+    @collage = 'shared/components/collage'
+
   end
 end
---
+++ test/controllers/shared/components_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Shared::ComponentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
